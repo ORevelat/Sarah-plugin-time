a0c537e93786f6dd5322977fc71dfdb57f479eb6
Olivier <olivier.revelat@gmail.com>
2017-01-20 06:45:04 +0100

    code formatting

--- time.js
+++ time.js
@@ -34,7 +34,7 @@ var actionTime = function (data, next) {
 	var date = new Date();
 
 	var text = 'il est ' + date.getHours() + ' heure ';
-	if (date.getMinutes() > 0){ 
+	if (date.getMinutes() > 0) { 
 		text += date.getMinutes();
 	}
 
